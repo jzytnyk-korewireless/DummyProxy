9f37de99468d55983d2a12dc8bbc4eb05255ff73
jzytnyk-korewireless <jzytnyk@koresystemsgroup.com>
2015-03-14 17:18:47 -0500

    added a second thread to send and receive a packet

--- DummyProxy/DummyProxy/Program.cs
+++ DummyProxy/DummyProxy/Program.cs
@@ -2,69 +2,97 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace dummyProxy
 {
     class Program
     {
-        private static readonly IPAddress RadiusServerIp = IPAddress.Any;
-        private const int RadiusPort = 1812;
-        private static Socket _radiusSocket;
-        private const int SIXTEEN_MEGABYTES = 16777216;
-
-        static void Main(string[] args)
+        static void Main()
         {
-            
+           // Console.WriteLine("Port to listen?");
+            const int listenPort = 55555; //Console.ReadLine();
             var buffer = new UdpPacketBuffer();
-            var counter = 0;
 
             // Create a UDP socket to listen for incoming authentication requests.
-            Console.WriteLine("Initializing authentication socket (IP {0}, port {1})...", RadiusServerIp, RadiusPort);
+            Console.WriteLine("main: Initializing authentication socket (IP {0}, port {1})...", IPAddress.Loopback, listenPort);
             try
             {
-                _radiusSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
-                {
-                    ReceiveBufferSize = SIXTEEN_MEGABYTES
-                };
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
-                _radiusSocket.Bind(new IPEndPoint(RadiusServerIp, RadiusPort));
+                socket.Bind(new IPEndPoint(IPAddress.Loopback, listenPort));
          
-                while(true)
-                {
-                    Console.WriteLine("waiting for packet #{0}",counter);
+                var thr = new Thread(Main2);
+                thr.Start();
 
-                    _radiusSocket.ReceiveFrom(buffer.Data, UdpPacketBuffer.BUFFER_SIZE, SocketFlags.None, ref buffer.RemoteEndPoint);
+                Console.WriteLine("main: waiting for packet");
 
-                    var remoteEndPoint = ((IPEndPoint) buffer.RemoteEndPoint);
-                    var ip = remoteEndPoint.Address;
-                    var port = remoteEndPoint.Port;
+                socket.ReceiveFrom(buffer.Data, ref buffer.RemoteEndPoint);
+
+                var remoteEndPoint = ((IPEndPoint) buffer.RemoteEndPoint);
+                var ip = remoteEndPoint.Address;
+                var port = remoteEndPoint.Port;
                     
-                    Console.WriteLine("Received from: {0}:{1} data: {2}",ip,port,Encoding.Default.GetString(buffer.Data));
-                    counter++;
-                }
+                Console.WriteLine("main: Received from: {0}:{1} data: {2}",ip,port,Encoding.Default.GetString(buffer.Data));
+
+                socket.SendTo(buffer.Data, remoteEndPoint);
+
+                Console.WriteLine("main: sent the bytes back");
+
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine("An error occurred at BeginReceiveFrom: {0}", ex.Message);
             }
-            
+            Console.ReadLine();
         }
-    }
 
+        static void Main2()
+        {
+            var socket2 = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var buffer = new UdpPacketBuffer();
+            Console.WriteLine("thread: sending msg..");
+            socket2.SendTo(Encoding.Default.GetBytes("weird message"),
+                new IPEndPoint(IPAddress.Loopback, 55555));
+
+            try
+            {
+
+                Console.WriteLine("thread: waiting for packet");
+
+                socket2.ReceiveFrom(buffer.Data, ref buffer.RemoteEndPoint);
+
+                var remoteEndPoint = ((IPEndPoint)buffer.RemoteEndPoint);
+                var ip = remoteEndPoint.Address;
+                var port = remoteEndPoint.Port;
+
+                Console.WriteLine("thread: Received from: {0}:{1} data: {2}", ip, port, Encoding.Default.GetString(buffer.Data));
+
+                socket2.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("thread: An error occurred at BeginReceiveFrom: {0}", ex.Message);
+            }
+            Console.ReadLine();
+        }
+    
+    }
 
     
     public class UdpPacketBuffer
     {
-        public const int BUFFER_SIZE = 128;
+        public const int BufferSize = 128;
         public byte[] Data;
         public int DataLength;
         public EndPoint RemoteEndPoint;
         
         public UdpPacketBuffer()
         {
-            this.Data = new byte[BUFFER_SIZE];
-            RemoteEndPoint = (EndPoint)new IPEndPoint(IPAddress.Any, 0);
+            Data = new byte[BufferSize];
+            RemoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
         }
     }
 }
